6160e41a0a0ac92ffd879fcec58ae6fc7406efaa
shubhammahale <shubhammahale19@gmail.com>
2026-04-24 00:49:45 +0530

    Implement user authentication and token generation endpoints

--- routers/auth.py
+++ routers/auth.py
@@ -6,6 +6,7 @@ from models import Users
 from database import SessionLocal 
 from passlib.context import CryptContext 
 from starlette import status
+from fastapi.security import OAuth2PasswordRequestForm
 
 router = APIRouter()
 
@@ -28,6 +29,16 @@ def get_db():
 
 db_dependency = Annotated[Session, Depends(get_db)]
 
+def authenticate_user(username: str, password:str,db:db_dependency):
+    user = db.query(Users).filter(Users.username== username).first()
+
+    if not user:
+        return False
+    if not bcrypt_context.verify(password,user.hashed_password):
+        return False
+    return True
+
+
 @router.post('/auth',status_code=status.HTTP_201_CREATED)
 async def create_user(db:db_dependency,
                       create_user_request: CreateUserRequest):
@@ -44,6 +55,15 @@ async def create_user(db:db_dependency,
 
     db.add(create_user_model)
     db.commit()
+
+@router.post("/token")
+async def login_for_access_token(form_data:Annotated[OAuth2PasswordRequestForm, Depends()],
+                                 db:db_dependency):
+    
+    user =authenticate_user(form_data.username,form_data.password,db)
+    if not user:
+        return "Failed Authentication"
+    return "Successful Authentication"
 
 
     
